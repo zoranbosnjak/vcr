db0fd70cc48df984847fb77f360de812976946ca
Zoran Bošnjak <zoran.bosnjak@sloveniacontrol.si>
2017-07-26 14:32:44 +0200

    fileWriter rotate output file

--- src/Common.hs
+++ src/Common.hs
@@ -7,6 +7,7 @@
 
 module Common
 ( logM
+, Log.Priority(..)
 , VcrOptions(..)
 , vcrOptions
 , syslogOptions
--- src/File.hs
+++ src/File.hs
@@ -5,12 +5,7 @@
 -- This module provides common File definitions.
 --
 
-module File {-
-( FileStore(..)
-, fileStoreOptions
-, File.appendFile
-, rotateFile
-) -} where
+module File where
 
 import           Control.Monad
 import           Pipes
@@ -18,15 +13,13 @@ import qualified Pipes.ByteString as PBS
 import qualified Options.Applicative as Opt
 import           Data.Monoid ((<>))
 import qualified Data.ByteString as BS
-import           System.IO as IO
-{-
-import           Data.List (isPrefixOf, sort)
+import qualified System.IO as IO
 import qualified Data.Time as Time
+import           Data.List (isPrefixOf, sort)
 import qualified Data.Time.Clock.POSIX
 import           System.Directory (renameFile, listDirectory, removeFile)
 import           System.FilePath ((</>), takeDirectory)
 import           System.Posix (getFileStatus, fileSize, accessTimeHiRes)
--}
 
 -- local imports
 import qualified Common as C
@@ -88,65 +81,64 @@ humanTime = Opt.eitherReader $ \arg -> do
         _         -> Left $ "cannot parse value `" ++ arg ++ "'"
 
 -- | File writer with 'rotation' support.
--- | TODO: implement file rotation
 fileWriter :: FileStore -> Maybe Rotate -> Consumer BS.ByteString IO ()
 fileWriter fs mRotate = forever $ do
     msg <- await
-    lift $ case filePath fs of
-        "-" -> BS.putStr msg
-        f -> BS.appendFile f msg
+    lift $ do
+        appendToFile fs msg
+        rotateResult <- maybe (return Nothing) (rotateFile fs) mRotate
+        case rotateResult of
+            Nothing -> return ()
+            Just (new, old) -> do
+                C.logM C.INFO $ "Output file rotated: " ++ new
+                forM_ old $ \i -> do
+                    C.logM C.INFO $ "Old file removed: " ++ i
+  where
+    appendToFile (FileStore "-") = BS.putStr
+    appendToFile (FileStore f) = BS.appendFile f
+
+    -- | Rotate a file if limit reached.
+    rotateFile :: FileStore -> Rotate -> IO (Maybe (FilePath, [FilePath]))
+    rotateFile (FileStore "-") _rotate = return Nothing
+    rotateFile (FileStore path) (Rotate keep mSize mTime) = do
+        nowUtc <- Time.getCurrentTime
+        nowPosix <- Data.Time.Clock.POSIX.getPOSIXTime
+        st <- getFileStatus path
+
+        let fileAge = nowPosix - accessTimeHiRes st
+
+            sizeLimit = maybe False ((fromIntegral . fileSize) st >=) mSize
+            timeLimit = case mTime of
+                Nothing -> False
+                Just time -> (toRational fileAge) >= (toRational time)
+
+        case (sizeLimit || timeLimit) of
+            False -> return Nothing
+            True -> do
+                let newName = path ++ "-" ++ tFormat nowUtc
+                renameFile path newName
+                removed <- cleanup
+                return $ Just (newName, removed)
+
+      where
+
+        tFormat = Time.formatTime
+            Time.defaultTimeLocale
+            (Time.iso8601DateFormat (Just "%H-%M-%S"))
+
+        cleanup = do
+            let dirName = takeDirectory path
+            content <- fmap (dirName </>) <$> listDirectory dirName
+            let oldFiles = drop keep . reverse . sort . filter myFiles $ content
+            mapM_ removeFile oldFiles
+            return oldFiles
+
+        myFiles = isPrefixOf path
 
 -- | File reader.
 -- TODO: Close the file when reading is done.
 fileReader :: FileStore -> Producer BS.ByteString IO ()
 fileReader fs = PBS.fromHandle =<< case filePath fs of
     "-" -> return IO.stdin
-    f -> (lift $ openFile f ReadMode)
-
-{-
--- | Append some data to a file.
-appendFile :: (Enc.Encodable msg) => FileStore -> [msg] -> IO ()
-appendFile (FileStore path fmt) messages = do
-    let s = Enc.encodeList fmt messages
-    case path of
-        "-" -> BSL.hPut System.IO.stdout s
-        _ -> BSL.appendFile path s
-
--- | Rotate a file if limit reached.
-rotateFile :: FileStore -> Int -> Maybe Integer -> Maybe Double
-    -> IO (Maybe (FilePath, [FilePath]))
-rotateFile (FileStore "-" _) _ _ _ = return Nothing
-rotateFile (FileStore path _) keep mSize mTime = do
-    nowUtc <- Time.getCurrentTime
-    nowPosix <- Data.Time.Clock.POSIX.getPOSIXTime
-    st <- getFileStatus path
-
-    let fileAge = nowPosix - accessTimeHiRes st
-
-        sizeLimit = maybe False ((fromIntegral . fileSize) st >=) mSize
-        timeLimit = case mTime of
-            Nothing -> False
-            Just time -> (toRational fileAge) >= (toRational time)
-
-    case (sizeLimit || timeLimit) of
-        False -> return Nothing
-        True -> do
-            let newName = path ++ "-" ++ tFormat nowUtc
-            renameFile path newName
-            removed <- cleanup
-            return $ Just (newName, removed)
-  where
-    tFormat = Time.formatTime
-        Time.defaultTimeLocale
-        (Time.iso8601DateFormat (Just "%H-%M-%S"))
-
-    cleanup = do
-        let dirName = takeDirectory path
-        content <- fmap (dirName </>) <$> listDirectory dirName
-        let oldFiles = drop keep . reverse . sort . filter myFiles $ content
-        mapM_ removeFile oldFiles
-        return oldFiles
-
-    myFiles = isPrefixOf path
--}
+    f -> (lift $ IO.openFile f IO.ReadMode)
 

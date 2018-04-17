d36b75aa04f7aaee967c3460fd8164e145a99cbb
Zoran Bošnjak <zoran.bosnjak@sloveniacontrol.si>
2018-04-17 15:02:45 +0200

    CmdServe limit to a single database

--- src/CmdServe.hs
+++ src/CmdServe.hs
@@ -9,29 +9,24 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE ExistentialQuantification #-}
 
---module CmdServe (cmdServe) where
-module CmdServe where
+module CmdServe (cmdServe) where
 
 -- Standard imports.
-import           Control.Concurrent.Async
-import qualified Control.Exception
---import           Control.Exception (SomeException)
-import           Control.Monad hiding (forever)
+import           Control.Exception (SomeException, bracket, try, onException)
 import           Control.Monad.IO.Class (liftIO)
 import           Options.Applicative ((<**>))
 import qualified Options.Applicative as Opt
-import           System.Log.Logger (Priority(DEBUG, INFO))
---import           Control.Concurrent (threadDelay)
---import           Data.Binary.Builder (fromByteString)
+import           System.Log.Logger (Priority(DEBUG, INFO, NOTICE))
+import           Control.Concurrent (threadDelay)
+import           Data.Binary.Builder (fromByteString)
 import           Data.Monoid
 import           Data.Convertible
 import           Database.HDBC as DB
 import           Database.HDBC.Sqlite3 (connectSqlite3)
 import           Database.HDBC.PostgreSQL (connectPostgreSQL)
---import           Data.Either
---import qualified Data.Text
---import qualified Data.ByteString as BS
---import           Text.Read (readMaybe)
+import qualified Data.Text
+import qualified Data.ByteString as BS
+import           Text.Read (readMaybe)
 import           Network.Wai
 import qualified Network.Wai.Handler.Warp as Warp
 import           Network.HTTP.Types
@@ -42,8 +37,8 @@ import qualified Common as C
 import qualified Event as E
 --import qualified Server
 --import qualified File
---import qualified Encodings
---import           Streams
+import qualified Encodings
+import           Streams
 
 cmdServe :: Opt.ParserInfo (C.VcrOptions -> IO ())
 cmdServe = Opt.info ((runCmd <$> CmdServe.options) <**> Opt.helper)
@@ -51,24 +46,17 @@ cmdServe = Opt.info ((runCmd <$> CmdServe.options) <**> Opt.helper)
 
 type Ip = String
 type Port = Int
-type RequestedReplicas = Int
---type ActualReplicas = Int
 
 data Options = Options
-    { optServe :: [Server]
-    , optStore :: [Store]
-    , optReplicas :: RequestedReplicas
+    { optServe :: Server
+    , optStore :: Store
     , optPrepare :: Bool
     } deriving (Eq, Show)
 
 options :: Opt.Parser CmdServe.Options
 options = CmdServe.Options
-    <$> Opt.some server
-    <*> Opt.some store
-    <*> Opt.option Opt.auto
-        (Opt.short 'n' <> Opt.long "replicas"
-            <> Opt.help "requested number of replicas before acknowledge write"
-            <> Opt.value 1)
+    <$> server
+    <*> store
     <*> Opt.switch
         (Opt.long "bootstrap"
             <> Opt.help "create database tables and exit")
@@ -126,28 +114,19 @@ runCmd opts vcrOpts = do
     logM INFO $
         "serve, opts: " ++ show opts ++ ", vcrOpts: " ++ show vcrOpts
 
-    C.check (optReplicas opts <= length (optStore opts))
-        "number of requested replicas must be <= available database connections"
-
-    let connectors = connector <$> (optStore opts)
+    let db = connector $ optStore opts
 
     case optPrepare opts of
         -- (re)create tables
         True -> do
-            logM INFO "preparing databases..."
-            forM_ connectors $ flip withDatabase createTables
-
+            logM INFO "preparing database..."
+            _ <- withDatabase db createTables
+            return ()
         -- normal operation (assume tables are prepared)
         False -> do
-            logM INFO "server..."
-            -- start servers
-            servers <- forM (optServe opts) $ \i -> liftIO $ async $ case i of
-                ServerHttp _ip port -> Warp.run port $
-                    app connectors (optReplicas opts)
-
-            -- all threads shall remain running
-            _ <- liftIO $ waitAnyCatchCancel servers
-            C.throw "server process terminated"
+            logM INFO "starting server"
+            case optServe opts of
+                ServerHttp _ip port -> Warp.run port $ app db
 
 -- | Connect to a database, run some action, auto commit and disconnect.
 withDatabase :: Connector -> (ConnWrapper -> IO a) -> IO a
@@ -197,10 +176,8 @@ toEvent [ch, src, utc, mono, ses, sn, val] = E.Event
     <*> safeFromSql val
 toEvent val = convError "unexpected number of columns" val
 
-app :: [Connector] -> Int -> Application
-app _connectors _replicas request respond = withLog go where
-
-    --threadDelaySec = threadDelay . round . (1000000*)
+app :: Connector -> Application
+app db request respond = withLog go where
 
     -- response check
     go ["ping"] GET = respond $ responseLBS
@@ -208,14 +185,16 @@ app _connectors _replicas request respond = withLog go where
         [("Content-Type", "text/plain")]
         "pong"
 
-    {-
-    -- simulate processing delay
+   -- to simulate processing delay, visit /delay/{seconds}
+   -- this url is just to confirm, that server is responding to
+   -- other requests, while servicing a long request
     go ["delay", s] GET = case readMaybe (Data.Text.unpack s) of
         Nothing -> respond $ responseLBS
             status400
             [("Content-Type", "text/plain")]
             "unable to decode delay value"
         Just (val::Double) -> do
+            let threadDelaySec = threadDelay . round . (1000000*)
             threadDelaySec val
             respond $ responseLBS
                 status200
@@ -226,59 +205,41 @@ app _connectors _replicas request respond = withLog go where
 
     -- TODO: add query params, see Data.Time.Format
     -- TODO: handle different content types
-    go ["events"] GET = do
-        -- connect to databases and run select
-        candidates <- forM connectors $ \(Connector name connect) -> do
-            Control.Exception.try $ do
-                conn <- ConnWrapper <$> connect
-                select <- prepare conn
-                    "SELECT * from events ORDER BY ch, sesId, monoTime ASC;"
-                _ <- execute select []
-                return (name, conn, select)
+    go ["events"] GET = withDatabase db $ \conn -> do
+        eSelect <- Control.Exception.try $ do
+            select <- prepare conn
+                "SELECT * from events ORDER BY ch, sesId, monoTime ASC;"
+            _ <- execute select []
+            return select
 
-        let inactive :: [SomeException]
-            inactive = lefts candidates
-
-        -- all active databases must be able to return values
-        case rights candidates of
-            [] -> do
-                logM NOTICE $ "no database is accessible"
-                    ++ ", " ++ show (length inactive) ++ " inactive databases"
+        case eSelect of
+            Left (_e :: SomeException) -> do
+                logM NOTICE $ "database error"
                 respond $ responseLBS
                     status503
                     [("Content-Type", "text/plain")]
                     "database error"
-            active -> do
-                let getName (name,_,_) = name
-                    getCon (_,con,_) = con
-                    getSelect (_,_,select) = select
-                logM DEBUG $
-                    "active databases: " ++ show (getName <$> active)
-                    ++ ", inactive: " ++ show (length inactive)
-                let src = mergeOrdStreams (toProducer . getSelect <$> active)
-                    action = respond $ responseStream
+            Right select -> do
+                let action = respond $ responseStream
                         status200
                         [("Content-Type", "text/plain")]
                         $ \write flush -> do
                             runStream $
-                                src
+                                toProducer select
                                 >-> Encodings.toByteString fmt
                                 >-> sender write
                             flush
-                            logM DEBUG $
-                                "closing database " ++ show (getName <$> active)
-                            mapM_ disconnect (getCon <$> active)
+                            logM DEBUG "closing database"
+                            disconnect conn
                     cleanup = do
                         logM NOTICE "streaming error"
-                        logM DEBUG $
-                            "closing database " ++ show (getName <$> active)
-                        mapM_ disconnect (getCon <$> active)
+                        logM DEBUG "closing database"
+                        disconnect conn
                         respond $ responseLBS
                             status503
                             [("Content-Type", "text/plain")]
                             "streaming error"
-                Control.Exception.onException action cleanup
-
+                onException action cleanup
       where
 
         fmt = Encodings.EncJSON Encodings.JSONCompact
@@ -301,23 +262,23 @@ app _connectors _replicas request respond = withLog go where
                         loop produce
 
         sender tx = mkConsumer $ \consume -> forever $ do
-            consume >>= liftIO . tx . fromByteString
+            consume Clear >>= liftIO . tx . fromByteString
 
     -- TODO: handle different content types
     -- TODO: maxSize param
-    -- TODO: run in parallel, use async
-    -- TODO: performance, finish when required number of replicas respond
     -- TODO: handle database exceptions
-    go ["events"] PUT = do
-        rv <- Control.Exception.try $ runStream $
+    go ["events"] PUT = withDatabase db $ \conn -> do
+        rv <- try $ runStream $
             reader
             >-> Encodings.fromByteString maxSize fmt
-            >-> forkStreams [writer c | c <- connectors]
+            >-> writer conn
         case rv of
-            Left (_ :: SomeException) -> respond $ responseLBS
-                status503
-                [("Content-Type", "text/plain")]
-                "database error"
+            Left (e :: SomeException) -> do
+                liftIO $ logM NOTICE $ "error: " ++ show e
+                respond $ responseLBS
+                    status503
+                    [("Content-Type", "text/plain")]
+                    "error"
             Right _ -> respond $ responseLBS
                 status200
                 [("Content-Type", "text/plain")]
@@ -336,34 +297,19 @@ app _connectors _replicas request respond = withLog go where
                         _ <- produce chunk
                         f produce
 
-        writer :: Connector
+        writer :: ConnWrapper
             -> Consumer (Either (String, BS.ByteString) E.Event)
-        writer (Connector name connect) = mkConsumer action where
-            acquire = do
-                liftIO $ logM DEBUG $ "open database " ++ show name
-                ConnWrapper <$> (liftIO connect)
-            release conn = liftIO $ do
-                rv <- Control.Exception.try $ commit conn
-                case rv of
-                    Left (e :: SomeException) -> logM NOTICE $
-                        "unable to commit messages to "
-                        ++ show name
-                        ++ ": " ++ show e
-                    Right _ -> return ()
-                logM DEBUG $ "closing database " ++ show name
-                disconnect conn
-            action consume = bracket acquire release $ \conn -> forever $ do
-                eVal <- consume
-                case eVal of
-                    Left (msg, _bs) -> liftIO $ logM NOTICE msg
-                    Right evt -> liftIO $ do
-                        _ <- deposit conn evt
-                        logM DEBUG $
-                            show (E.eChannel evt, E.eUtcTime evt)
-                            ++ " saved."
+        writer conn = mkConsumer $ \consume -> forever $ do
+            eVal <- consume Clear
+            case eVal of
+                Left (msg, _bs) -> fail msg
+                Right evt -> liftIO $ do
+                    _ <- deposit conn evt
+                    logM DEBUG $
+                        show (E.eChannel evt, E.eUtcTime evt)
+                        ++ " saved."
 
     go ["events"] DELETE = undefined
-    -}
 
     -- not found
     go _ _ = respond $ responseLBS
